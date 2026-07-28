c004980bfcc1793236c64d625373d217b3171745
Jenni <tangjennii@gmail.com>
2026-07-28 18:03:28 -0400

    hotels: photo-led cards, badge off the photo, collapsible neighbourhood filter; note form opens with a line

--- notes.js
+++ notes.js
@@ -11,6 +11,7 @@
 
   function formHTML(p, intro){
     return ('' +
+      '<p class="note-top">Share some wild memories, feels, etc. with us \u2661</p>' +
       (intro ? '<p class="note-intro">' + intro + '</p>' : '') +
       '<div class="note-field">' +
         '<label class="note-l" for="{p}Mem">a favorite memory with Sam and/or Jenni</label>' +
